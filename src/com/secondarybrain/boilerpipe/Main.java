480799eb2b397dab135b464ac9cf6ed7efcfba2a
Sumin Byeon <suminb@gmail.com>
2013-04-29 23:10:44 -0700

    Code formatting.

--- src/com/secondarybrain/boilerpipe/Main.java
+++ src/com/secondarybrain/boilerpipe/Main.java
@@ -1,4 +1,5 @@
 package com.secondarybrain.boilerpipe;
+
 import gnu.getopt.Getopt;
 
 import java.io.BufferedReader;
@@ -15,41 +16,43 @@ import de.l3s.boilerpipe.BoilerpipeProcessingException;
 import de.l3s.boilerpipe.extractors.ArticleExtractor;
 
 public class Main {
-	
+
 	public static enum Mode {
 		URL, File
 	}
-	
+
 	private Mode mode;
 	private String uri;
-	
+
 	private void init(String[] args) throws Exception {
-    	Getopt g = new Getopt("boilerpipe", args, "u:f:");
-    	
-    	int c = 0;
-    	while ((c = g.getopt()) != -1) {
-    		switch(c) {
-    		case 'u':
-    			mode = Mode.URL;
-    			uri = g.getOptarg();
-    			break;
-    		
-    		case 'f':
-    			mode = Mode.File;
-    			uri = g.getOptarg();
-    			break;
-    		}
-    	}
-    	
-    	if (mode != null) {
-    		execute();
-    	}
+		Getopt g = new Getopt("boilerpipe", args, "u:f:");
+
+		int c = 0;
+		while ((c = g.getopt()) != -1) {
+			switch (c) {
+			case 'u':
+				mode = Mode.URL;
+				uri = g.getOptarg();
+				break;
+
+			case 'f':
+				mode = Mode.File;
+				uri = g.getOptarg();
+				break;
+			}
+		}
+
+		if (mode != null) {
+			execute();
+		}
 	}
-	
-	private void execute() throws FileNotFoundException, BoilerpipeProcessingException, MalformedURLException, UnsupportedEncodingException {
+
+	private void execute() throws FileNotFoundException,
+			BoilerpipeProcessingException, MalformedURLException,
+			UnsupportedEncodingException {
 		String text = null;
 		if (Mode.File.equals(mode)) {
-			//Reader reader = new InputStreamReader(new FileInputStream(uri), "UTF-8");
+			// Reader reader = new InputStreamReader(new FileInputStream(uri), "UTF-8");
 			Reader reader = new FileReader(uri);
 			text = ArticleExtractor.INSTANCE.getText(reader);
 		}
@@ -59,7 +62,7 @@ public class Main {
 		System.out.println(text);
 	}
 
-    public static void main(String[] args) throws Exception {
-    	new Main().init(args);
-    }
+	public static void main(String[] args) throws Exception {
+		new Main().init(args);
+	}
 }
